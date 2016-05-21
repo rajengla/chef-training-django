213240a3f01fce357b27c6ed9077204cb6be7e17
Raj Engla <raj89892@gmail.com>
2016-05-21 13:51:03 +0530

    implement installation by packages

--- recipes/install_from_packages.rb
+++ recipes/install_from_packages.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: django
-# Recipe:: default
+# Recipe:: install_from_packages
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-include_recipe 'django::install_from_packages'
+package 'python-django'
